c62f2be2f14f10657be5e75ee1200aa57b953568
我可能很廢 <nicks96432@gmail.com>
2024-07-22 16:12:33 +0800

    :recycle: Remove depecrated options and change background

--- .zshrcs/init.zsh
+++ .zshrcs/init.zsh
@@ -15,10 +15,10 @@ unsetopt autocd beep
 
 export PATH=$HOME/bin:$HOME/.local/bin:$HOME/go/bin:$HOME/node/bin:$HOME/.local/share/gem/ruby/3.0.0/bin:$HOME/.cargo/bin:/opt/osxcross/bin:/opt/cuda/bin:/opt/cuda/nsight_systems/bin:/opt/cuda/nsight_compute:$PATH
 export npm_config_prefix=$HOME/.local
-export DOCKER_HOST=unix:///run/docker.sock
 export OptiX_INSTALL_DIR=/opt/optix
 
 if command -v sccache >/dev/null 2>&1; then
     export RUSTC_WRAPPER=sccache
 fi
+
 ZIM_CONFIG_FILE=$ZSH_CONFIG_DIR/.zimrc
